dd3c0a396b94abc6c19d87a1a571cf65b37ff75a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-11-27 10:33:24 +0300

    task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,9 @@
+# 2-execute_a_command.pp
 #create a manifest that kills a process named killmenow
 # Must use the exec Puppet resource
 # Must use pkill
 
-exec { 'killing':
+exec { 'killmenow':
   command => '/usr/bin/pkill killmenow',
   onlyif  => '/usr/bin/pgrep -f "killmenow"',
 } 
