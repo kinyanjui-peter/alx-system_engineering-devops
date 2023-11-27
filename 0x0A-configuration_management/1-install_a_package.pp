5c256ef73a5fe2cd9203384982be8daa30457d01
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-11-27 10:53:26 +0300

    task2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # install frask 2.1.0 from pip3 
-
+class {'python3':
+  path   => 'usr/bin/python3.8',
+  ensure => installed,
+  
 exec { 'install_flask':
-  command   => '2.1.0',
-   provider => 'pip3',
+  command   =>  '/usr/bin/python3.8 -m pip install flask==2.1.0',
+  unless    => '/usr/bin/python3.8 -m pip show Flask | grep -q "Version: 2.1.0"',
 }
