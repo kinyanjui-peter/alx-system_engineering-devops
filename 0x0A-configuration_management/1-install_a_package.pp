0f593c7d190eeff8f34074315395fa44250cb70a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-11-27 11:17:13 +0300

    task2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,7 @@ class {'python3':
 }
   
 exec { 'install_flask':
-  command   => '/usr/bin/pip3 install flask==2.1.0',
-  onlyif    => '/usr/bin/pip3 show flask"',
+  command   => 'install flask==2.1.0',
+  path      => '/usr/bin/pip3',
+  onlyif    => '/usr/bin/pip3 show flask',
 }
