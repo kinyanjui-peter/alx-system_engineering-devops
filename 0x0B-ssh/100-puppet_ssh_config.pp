15419f31e084cf90fee8f3363febb24709ab93be
kinyanjui-peter <01peterkinyanjui@gmail.com>
2023-12-06 23:41:38 +0300

    task100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # ssh_config.pp
 
 # Ensure the SSH client configuration is set up
-file { '/home/your_username/.ssh/config':
+file { '~/.ssh/config':
   ensure  => 'present',
   content => 'IdentityFile ~/.ssh/school
       	      PasswordAuthentication no',
