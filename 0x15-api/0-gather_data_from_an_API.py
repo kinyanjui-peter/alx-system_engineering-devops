63a9286e2f3c81af29a3f15171c86981fdc7d7ee
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-03 08:53:18 +0300

    task3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 import sys
 import requests
-
+"""Python script that, using this REST API, for a given employee ID,
+        returns information about his/her TODO list progress.
+    ou must use urllib or requests module
+The script must accept an integer as a parameter, which is the employee ID
+The script must display on the standard output the employee TODO list progress in this exact format:
+First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+EMPLOYEE_NAME: name of the employee
+NUMBER_OF_DONE_TASKS: number of completed tasks
+TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
+Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+"""
 def gather_data(employee_id):
     # URL for the JSONPlaceholder API's user endpoint
     url_user = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
