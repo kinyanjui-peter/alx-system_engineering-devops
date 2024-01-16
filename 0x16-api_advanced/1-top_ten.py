a0da88c84420c21d6e1c69be830612692b75659f
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-17 00:36:47 +0300

    task0

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ A function that gets top 10 reddits"""
+    url = f"https://www.reddit.com/{subreddit}/hot.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        
+        for post in data.get("data", {}).get("children", [])[:10]:
+            title = post.get("data", {}).get("title", "")
+            print(title)
+    elif response.status_code == 404:
+        return f"None"
+    else:
+        return f"None"
