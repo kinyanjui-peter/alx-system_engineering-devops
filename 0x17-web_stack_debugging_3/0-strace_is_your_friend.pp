4f984fc98680efb97857a1c7dcbd2df0170890f7
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-18 05:07:23 +0300

    task0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes Apache 500 error and automating puppet manifest
+
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}star
