12245c31c06684e6796438b78d25e09129d84513
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-27 22:31:47 +0300

    web stack debugging-apachebenchmark

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,10 +4,10 @@
 # Exec resource to adjust ULIMIT in the Nginx configuration file
 exec { 'fix_for_nginx_ulimit':
   command => '/bin/sed -i "s/15/4000/" /etc/default/nginx',
-  
+
   # Replace the value '15' with '4000' in the nginx configuration file
   path    => ['/usr/local/bin/', '/bin/'],
-  
+
   # Set the PATH environment variable for the command execution
   notify  => Exec['nginx_restart'],
   # Notify the 'nginx_restart' exec resource to restart Nginx after
