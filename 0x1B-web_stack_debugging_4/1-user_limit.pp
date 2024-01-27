caf6da58de4f99522065df2e21d5220872f74ecc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-01-27 22:54:32 +0300

    web stack debugging-apachebenchmark

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,12 @@
 # enable user holbetrton to login without error
-# increase hard-files-for-holbetrton
+# increase hard files for holbetrton
 exec { 'increase_hard_file_for_user_holberton':
-    command => "sed -i '/^holbetrton hard/s/5/5000/' /etc/security/limits.conf",
-    path => '/usr/local/bin/:/bin/',
+    command => "sed -i '/^holberton hard/s/5/5000/' /etc/security/limits.conf",
+    path    => '/usr/local/bin/:/bin/',
 }
-exec {'increse-soft-filr-for-holbetrton':
-command => 'sed -i "holbetrton soft/s/4/5000" /etc/security/limits.conf',
-path => '/usr/local/bin/:/bin/'
+
+# increase soft files for holbetrton
+exec { 'increase_soft_file_for_holberton':
+    command => 'sed -i "/^holberton soft/s/4/5000/" /etc/security/limits.conf',
+    path    => '/usr/local/bin/:/bin/',
 }
